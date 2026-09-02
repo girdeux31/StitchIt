21596d86d200d21dead62d5872be4b8deaa67d70
girdeux31 <mesado31@gmail.com>
2026-09-02 23:53:55 +0200

    tests

--- tests/base_test.py
+++ tests/base_test.py
@@ -20,15 +20,15 @@ def compare_files(file_a: Path, file_b: Path):
 
 def assert_output(inp_name: Path, tmp_file: Path):
     """Check that output files exists and that svg and txt don't change"""
-    pdf_file = tmp_file.with_stem(f'{tmp_file.stem}_chart').with_suffix('.pdf')
-    png_file = tmp_file.with_stem(f'{tmp_file.stem}_chart').with_suffix('.png')
+    # pdf_file = tmp_file.with_stem(f'{tmp_file.stem}_chart').with_suffix('.pdf')
+    # png_file = tmp_file.with_stem(f'{tmp_file.stem}_chart').with_suffix('.png')
     svg_file = tmp_file.with_stem(f'{tmp_file.stem}_chart').with_suffix('.svg')
     txt_file = tmp_file.with_stem(f'{tmp_file.stem}_info').with_suffix('.txt')
     svg_ref_file = REF_PATH / inp_name.stem / svg_file.name
     txt_ref_file = REF_PATH / inp_name.stem / txt_file.name
 
-    assert pdf_file.exists()
-    assert png_file.exists()
+    # assert pdf_file.exists()
+    # assert png_file.exists()
     assert svg_file.exists()
     assert txt_file.exists()
     assert compare_files(svg_ref_file, svg_file)
@@ -62,6 +62,8 @@ def _setup_args(ref_file: Path, args: dict[str, str | int | float]) -> None:
     """Set user input parameters as sys arguments"""
     del sys.argv[1:]  # reset args
     args['input-file'] = str(ref_file)
+    args['no-pdf'] = None  # do not generate pdf or png
+    args['no-png'] = None
     for key, value in args.items():
         sys.argv.append(f'--{key}')
         if value is not None:
